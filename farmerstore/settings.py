1b11bb52b49b7fdeb8846a6aab7ad1505f96f7cb
Anthony Aruma <anthonyaruma091@gmail.com>
2024-09-30 16:52:31 +0100

    Created the register, login and logout page

--- farmerstore/settings.py
+++ farmerstore/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
+
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
